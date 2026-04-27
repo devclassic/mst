cd8523259bbe4046e3c22d31c2392d182c02ad0d
devclassic <devclassic@hotmail.com>
2026-04-27 09:10:50 +0800

    配置项目和UI库

--- mst-ui/src/main.js
+++ mst-ui/src/main.js
@@ -1,10 +1,11 @@
-import {
-	createSSRApp
-} from "vue";
-import App from "./App.vue";
+import { createSSRApp } from 'vue'
+import uViewPro from 'uview-pro'
+import App from './App.vue'
+
 export function createApp() {
-	const app = createSSRApp(App);
-	return {
-		app,
-	};
+  const app = createSSRApp(App)
+  app.use(uViewPro)
+  return {
+    app,
+  }
 }
